3722fd24a4b55f965073b6c9644b7f1226486949
ass3m <42045481+assemnabill@users.noreply.github.com>
2025-08-26 20:42:00 +0200

    Update dependencies and introduce Docker integration
    Replaced the `docker-compose.yml` configuration with new centralized artifacts to support Docker-based hosting. Upgraded multiple dependencies and SDK versions for Redis, PostgreSQL, Mapster, and Aspire components to their latest stable or preview versions. Added Redis and Postgres container configuration for improved deployment flexibility.

--- Hodler.AppHost/Program.cs
+++ Hodler.AppHost/Program.cs
@@ -2,17 +2,25 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("cache");
+builder.AddDockerComposeEnvironment("compose");
+
+var cache = builder
+    .AddRedis("cache")
+    .WithContainerName("redis-cache")
+    .WithImageTag("7-alpine"); // ARM-compatible tag
 
 const string serviceName = "hodler";
 const string apiServiceName = $"{serviceName}-api";
 const string dbName = $"{serviceName}-db";
 
 var postgresComponent = builder
-    .AddPostgres("postgres")
-    .WithDataVolume();
+    .AddPostgres("postgres", password: builder.AddParameter("postgres-password"))
+    .WithContainerName(dbName)
+    .WithDataVolume($"{dbName}-vol")
+    .WithImageTag("15-alpine"); // ARM-compatible tag
 
-var hodlerDb = postgresComponent.AddDatabase(dbName);
+var hodlerDb = postgresComponent
+    .AddDatabase(dbName);
 
 var apiService = builder
     .AddProject<Hodler_ApiService>(apiServiceName)
